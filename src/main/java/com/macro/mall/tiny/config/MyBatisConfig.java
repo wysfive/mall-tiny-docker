f4a2760dccbc109b7ef25ef05d43a0963766dc74
yasa <1394674558@qq.com>
2021-03-10 23:52:46 +0800

    cors跨域设置

--- src/main/java/com/macro/mall/tiny/config/MyBatisConfig.java
+++ src/main/java/com/macro/mall/tiny/config/MyBatisConfig.java
@@ -1,6 +1,10 @@
 package com.macro.mall.tiny.config;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import javafx.scene.control.Pagination;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -8,6 +12,13 @@ import org.springframework.context.annotation.Configuration;
  * Created by macro on 2019/4/8.
  */
 @Configuration
-@MapperScan({"com.macro.mall.tiny.mbg.mapper","com.macro.mall.tiny.dao"})
+@MapperScan({"com.macro.mall.tiny.mbg.mapper","com.macro.mall.tiny.dao","com.macro.mall.tiny.modules.*.mapper"})
 public class MyBatisConfig {
+    // 配置分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
 }
---
+++ src/test/java/com/macro/mall/tiny/MyBatisPlusGenerator.java
@@ -0,0 +1,12 @@
+package com.macro.mall.tiny;
+
+public class MyBatisPlusGenerator {
+    public static void main(String[] args) {
+        String projectPath = System.getProperty("user.dir") + "/mall-tiny-plus";
+        String moduleName = scanner("模块名");
+    }
+
+    private static String scanner(String tip) {
+        return null;
+    }
+}
